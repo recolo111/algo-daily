e2c95b657ba57404051b84c4351fc998f0f95045
recolo111 <151364173+recolo111@users.noreply.github.com>
2023-11-20 22:14:56 +0800

    recolo111
    # [NOIP2016 普及组] 买铅笔
    
    ## 题目背景
    
    NOIP2016 普及组 T1
    
    ## 题目描述
    
    P 老师需要去商店买 $n$ 支铅笔作为小朋友们参加 NOIP 的礼物。她发现商店一共有 $3$ 种包装的铅笔，不同包装内的铅笔数量有可能不同，价格也有可能不同。为了公平起 见，P 老师决定只买同一种包装的铅笔。
    
    商店不允许将铅笔的包装拆开，因此 P 老师可能需要购买超过 $n$ 支铅笔才够给小朋友们发礼物。
    
    现在 P 老师想知道，在商店每种包装的数量都足够的情况下，要买够至少 $n$ 支铅笔最少需要花费多少钱。
    
    ## 输入格式
    
    第一行包含一个正整数 $n$，表示需要的铅笔数量。
    
    接下来三行，每行用 $2$ 个正整数描述一种包装的铅笔：其中第 $1$ 个整数表示这种包装内铅笔的数量，第 $2$ 个整数表示这种包装的价格。
    
    保证所有的 $7$ 个数都是不超过 $10000$ 的正整数。
    
    ## 输出格式
    
    $1$ 个整数，表示 P 老师最少需要花费的钱。
    
    ## 样例 #1
    
    ### 样例输入 #1
    
    ```
    57
    2 2
    50 30
    30 27
    ```
    
    ### 样例输出 #1
    
    ```
    54
    ```
    
    ## 样例 #2
    
    ### 样例输入 #2
    
    ```
    9998
    128 233
    128 2333
    128 666
    ```
    
    ### 样例输出 #2
    
    ```
    18407
    ```
    
    ## 样例 #3
    
    ### 样例输入 #3
    
    ```
    9999
    101 1111
    1 9999
    1111 9999
    ```
    
    ### 样例输出 #3
    
    ```
    89991
    ```
    
    ## 提示
    
    铅笔的三种包装分别是：
    
    - $2$ 支装，价格为 $2$;
    - $50$ 支装，价格为 $30$;
    - $30$ 支装，价格为 $27$。
    
    P老师需要购买至少 $57$ 支铅笔。
    
    如果她选择购买第一种包装，那么她需要购买 $29$ 份，共计 $2 \times 29 = 58$ 支，需要花费的钱为 $2 \times 29 = 58$。
    
    实际上，P 老师会选择购买第三种包装，这样需要买 $2$ 份。虽然最后买到的铅笔数量更多了，为 $30 \times 2 = 60$ 支，但花费却减少为 $27 \times 2 = 54$，比第一种少。
    
    对于第二种包装，虽然每支铅笔的价格是最低的，但要够发必须买 $2$ 份，实际的花费达到了 $30  \times 2 = 60$，因此 P 老师也不会选择。
    
    所以最后输出的答案是 $54$。
    
    【数据范围】
    
    保证所有的 $7$ 个数都是不超过 $10000$ 的正整数。
    
    【子任务】
    
    子任务会给出部分测试数据的特点。如果你在解决题目中遇到了困难，可以尝试只解决一部分测试数据。
    
    每个测试点的数据规模及特点如下表：
    
     ![](https://cdn.luogu.com.cn/upload/pic/3449.png)
    
    上表中“整倍数”的意义为：若为 $K$，表示对应数据所需要的铅笔数量 $n$ —定是每种包装铅笔数量的整倍数（这意味着一定可以不用多买铅笔）。
    
    于 2022 年 12 月 23 日新加 Hack 数据三组。

--- recolo111/未命名1.cpp
+++ recolo111/未命名1.cpp
@@ -1,34 +1,14 @@
 #include<stdio.h>
 int main()
-{
-	int n=0 ;
-	scanf("%d",&n);
-	int a[5], b[5], c[5];
-	
-	for(int i = 0; i < 3; i++){
-		scanf("%d %d", &a[i], &b[i]);
-		//printf("a[i]=%d b[i]=%d ", a[i], b[i]); 
-			if(n % a[i] > 0){
-				c[i] = n / a[i] + 1;
-				c[i] = b[i] * c[i];
-                //printf("c[i]=%d ",c[i]);
-	    	}
-	    	else{
-	    		c[i] = n / a[i];
-	    		c[i] = b[i] * c[i];
-                //printf("c[i]=%d ",c[i]);
-			}
-		}
-	
-	int min=0;
-	min=c[0];
-	//printf("min=%d ",min);
-	for(int i = 0; i < 3; i++){
-	    if(c[i] < min){
-	   	      min=c[i]; 
-		} 
-	     
-	}
-	printf("%d", min);
-	return 0 ;
+{   double s, you, j;
+    int bu;
+    scanf("%1lf", &s);
+       for( j = 2.00, bu = 1, you = 0.00; you < s; ){
+          you = you + j;
+          bu++;
+          if(you < s)
+          j = j * 100 / 98;
+          //printf("j=%2lf you=%2lf\n", j, you);
+        }
+    printf("%d", bu);
 }
