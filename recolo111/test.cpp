b719c5dba1e8f1ba087df4454a726466d59970d1
recolo111 <151364173+recolo111@users.noreply.github.com>
2023-11-21 22:14:22 +0800

    recolo111:
    # [NOIP1998 普及组] 三连击
    
    ## 题目背景
    
    本题为提交答案题，您可以写程序或手算在本机上算出答案后，直接提交答案文本，也可提交答案生成程序。
    
    ## 题目描述
    
    将 $1, 2, \ldots , 9$ 共 $9$ 个数分成 $3$ 组，分别组成 $3$ 个三位数，且使这 $3$ 个三位数构成 $1 : 2 : 3$ 的比例，试求出所有满足条件的 $3$ 个三位数。
    
    ## 输入格式
    
    无
    
    ## 输出格式
    
    若干行，每行 $3$ 个数字。按照每行第 $1$ 个数字升序排列。
    
    ## 样例 #1
    
    ### 样例输入 #1
    
    ```
    无
    ```
    
    ### 样例输出 #1
    
    ```
    192 384 576
    * * *
    ...
    
    * * *
    （剩余部分不予展示）
    ```

--- recolo111/test.cpp
+++ recolo111/test.cpp
@@ -1,23 +1,46 @@
 #include<stdio.h>
-int main()
-{ int i,a,b,max,answer;
-  int x[7];
-  for(i=0;i<7;i++){
-    scanf("%d %d",&a,&b);
-    x[i]=a+b;}
-  for(max=0,i=6;i>=0;i--){
-    if(x[i]>=max&&x[i]>=8)
-    max=x[i];
-    else if(i==0&&x[i]<8)
-    answer=0;
+int a[10];
+int sum(int i,int a[])
+	{
+		for(int b = 0; i > 0; ){
+               b = i % 10;
+               //printf("b=%d ", b);
+			   a[10]=1;
+			   if(b == 0)
+               a[10]=0;
+			   i = i / 10;
+			   //printf("i=%d ",i);
+	              for(int c = 1; c <= 9; c++){
+                     if(a[c] == b){
+                        a[c] = 0;
+                        //printf("Ñ­»·Àïa[c]=%d ", a[c]);
+                        break;
+					  }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+	                }
+	   }
+	   return 0;
 	}
-  //printf("%d",max);
-  for(i=6;i>=0;i--){
-  	if(max==0)
-  	break;
-    else if(x[i]==max)
-    answer=i+1;}
-  printf("%d",answer);
-  return 0;
- }    
-
+	
+int main()
+{   int   i;
+    int x, y, z, b;
+    
+    for(int i = 123;i <= 333; i++){
+	 a[1] = 1, a[2] = 2, a[3] = 3;
+	 a[4] = 4, a[5] = 5, a[6] = 6;
+	 a[7] = 7, a[8] = 8, a[9] = 9;
+       x = i;
+	   y = 2 * i;
+	   z = 3 * i;
+	   sum(x,a);
+	   sum(y,a);
+	   sum(z,a);
+	   if(a[10]==0)
+	   continue;
+	      //printf("a[1]=%d ",a[1]);
+	         if(a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]==0){
+	            printf("%d %d %d\n", x, y, z);
+	         }
+    }
+	return 0 ;
+}
--- recolo111/未命名1.cpp
+++ recolo111/未命名1.cpp
@@ -1,14 +1,19 @@
 #include<stdio.h>
 int main()
-{   double s, you, j;
-    int bu;
-    scanf("%1lf", &s);
-       for( j = 2.00, bu = 1, you = 0.00; you < s; ){
+{   double s, you, j=2;
+    int bu = 0;
+    you = 0.00;
+    scanf("%lf", &s);
+    if(s <= 2){
+    printf("1");
+	}
+       for( ; you < s; ){
           you = you + j;
           bu++;
           if(you < s)
-          j = j * 100 / 98;
-          //printf("j=%2lf you=%2lf\n", j, you);
+          j = (j * 98)/ 100;
+          printf("j=%2lf you=%2lf\n", j, you);
         }
     printf("%d", bu);
+    return 0;
 }
