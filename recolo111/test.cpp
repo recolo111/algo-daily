f4d5a2f428be242b7057804f5c854acb03ed1748
recolo111 <151364173+recolo111@users.noreply.github.com>
2023-11-22 21:06:10 +0800

    recolo1111:
    # [NOIP2005 普及组] 陶陶摘苹果
    
    ## 题目描述
    
    陶陶家的院子里有一棵苹果树，每到秋天树上就会结出 $10$ 个苹果。苹果成熟的时候，陶陶就会跑去摘苹果。陶陶有个 $30$ 厘米高的板凳，当她不能直接用手摘到苹果的时候，就会踩到板凳上再试试。
    
    现在已知 $10$ 个苹果到地面的高度，以及陶陶把手伸直的时候能够达到的最大高度，请帮陶陶算一下她能够摘到的苹果的数目。假设她碰到苹果，苹果就会掉下来。
    
    ## 输入格式
    
    输入包括两行数据。第一行包含 $10$ 个 $100$ 到 $200$ 之间（包括 $100$ 和 $200$ ）的整数（以厘米为单位）分别表示 $10$ 个苹果到地面的高度，两个相邻的整数之间用一个空格隔开。第二行只包括一个 $100$ 到 $120$ 之间（包含 $100$ 和 $120$ ）的整数（以厘米为单位），表示陶陶把手伸直的时候能够达到的最大高度。
    
    ## 输出格式
    
    输出包括一行，这一行只包含一个整数，表示陶陶能够摘到的苹果的数目。
    
    ## 样例 #1
    
    ### 样例输入 #1
    
    ```
    100 200 150 140 129 134 167 198 200 111
    110
    ```
    
    ### 样例输出 #1
    
    ```
    5
    ```
    
    ## 提示
    
    **【题目来源】**
    
    NOIP 2005 普及组第一题

--- recolo111/test.cpp
+++ recolo111/test.cpp
@@ -1,46 +1,19 @@
 #include<stdio.h>
-int a[10];
-int sum(int i,int a[])
-	{
-		for(int b = 0; i > 0; ){
-               b = i % 10;
-               //printf("b=%d ", b);
-			   a[10]=1;
-			   if(b == 0)
-               a[10]=0;
-			   i = i / 10;
-			   //printf("i=%d ",i);
-	              for(int c = 1; c <= 9; c++){
-                     if(a[c] == b){
-                        a[c] = 0;
-                        //printf("Ñ­»·Àïa[c]=%d ", a[c]);
-                        break;
-					  }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-	                }
-	   }
-	   return 0;
-	}
-	
 int main()
-{   int   i;
-    int x, y, z, b;
-    
-    for(int i = 123;i <= 333; i++){
-	 a[1] = 1, a[2] = 2, a[3] = 3;
-	 a[4] = 4, a[5] = 5, a[6] = 6;
-	 a[7] = 7, a[8] = 8, a[9] = 9;
-       x = i;
-	   y = 2 * i;
-	   z = 3 * i;
-	   sum(x,a);
-	   sum(y,a);
-	   sum(z,a);
-	   if(a[10]==0)
-	   continue;
-	      //printf("a[1]=%d ",a[1]);
-	         if(a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]==0){
-	            printf("%d %d %d\n", x, y, z);
-	         }
+{   
+	int a[10], i;
+	for( i = 0; i < 10; i++){
+	  scanf("%d", &a[i]);
+	  //printf("a[i]=%d\n",a[i]) ;
     }
-	return 0 ;
+	  int b, ant;
+	  scanf("%d", &b);
+	  //printf("b=%d\n",b) ;
+	for(i = 0; i < 10; i++){
+	    if(b + 30 >= a[i])
+		ant++;
+		}
+		printf("%d", ant);
+	return 0;
 }
+
--- recolo111/未命名1.cpp
+++ recolo111/未命名1.cpp
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main()
-{   double s, you, j=2;
-    int bu = 0;
-    you = 0.00;
-    scanf("%lf", &s);
-    if(s <= 2){
-    printf("1");
-	}
-       for( ; you < s; ){
-          you = you + j;
-          bu++;
-          if(you < s)
-          j = (j * 98)/ 100;
-          printf("j=%2lf you=%2lf\n", j, you);
-        }
-    printf("%d", bu);
-    return 0;
+{   
+	int a[10], i;
+	for( i = 0; i < 10; i++){
+	  scanf("%d", &a[i]);
+	  //printf("a[i]=%d\n",a[i]) ;
+    }
+	  int b, ant;
+	  scanf("%d", &b);
+	  //printf("b=%d\n",b) ;
+	for(i = 0; i < 10; i++){
+	    if(b + 30 >= a[i])
+		ant++;
+		}
+		printf("%d", ant);
+	return 0;
 }
+
