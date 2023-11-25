89db084f075c97aa91a1af3888c1904451661492
recolo111 <151364173+recolo111@users.noreply.github.com>
2023-11-25 21:50:35 +0800

    recolo111:p1478
    # 陶陶摘苹果（升级版）
    
    ## 题目描述
    
    又是一年秋季时，陶陶家的苹果树结了 $n$ 个果子。陶陶又跑去摘苹果，这次他有一个 $a$ 公分的椅子。当他手够不着时，他会站到椅子上再试试。
    
    这次与 NOIp2005 普及组第一题不同的是：陶陶之前搬凳子，力气只剩下 $s$ 了。当然，每次摘苹果时都要用一定的力气。陶陶想知道在 $s<0$ 之前最多能摘到多少个苹果。
    
    现在已知 $n$ 个苹果到达地上的高度 $x_i$，椅子的高度 $a$，陶陶手伸直的最大长度 $b$，陶陶所剩的力气 $s$，陶陶摘一个苹果需要的力气 $y_i$，求陶陶最多能摘到多少个苹果。
    
    ## 输入格式
    
    第 $1$ 行：两个数 苹果数 $n$，力气 $s$。
    
    第 $2$ 行：两个数 椅子的高度 $a$，陶陶手伸直的最大长度 $b$。
    
    第 $3$ 行~第 $3+n-1$ 行：每行两个数 苹果高度 $x_i$，摘这个苹果需要的力气 $y_i$。
    
    ## 输出格式
    
    只有一个整数，表示陶陶最多能摘到的苹果数。
    
    ## 样例 #1
    
    ### 样例输入 #1
    
    ```
    8 15
    20 130
    120 3
    150 2
    110 7
    180 1
    50 8
    200 0
    140 3
    120 2
    ```
    
    ### 样例输出 #1
    
    ```
    4
    ```
    
    ## 提示
    
    对于 $100\%$ 的数据，$n\leq 5000$, $a\leq 50$, $b\leq 200$, $s\leq 1000$, $x_i\leq 280$, $y_i\leq 100$。

--- recolo111/test.cpp
+++ recolo111/test.cpp
@@ -1,30 +1,57 @@
 #include<stdio.h>
-int main(){
-	int N, anwser = 0, ans = 0;
-	int a[10];
-	scanf("%d", &N);
+int main()
+{ 
+	int n, s;
+	scanf("%d %d", &n, &s);//苹果数 n，力气 s。
+	int a, b;
+	//int c;
+	scanf("%d %d", &a, &b);//椅子的高度 a，陶陶手伸直的最大长度 b。
+	//c = a + b;//c是陶陶能碰到最大高度
+	int x[5010], y[5010];
 
-		for(int i = 0; ; i++){
-			if(N == 0){
+		for(int i = 0; i < n; i++){
+			scanf("%d %d",&x[i], &y[i]);
+			//printf("x[i]=%d y[i]=%d\n", x[i], y[i]);
+		}
+	//printf("x[0]=%d y[0]=%d\n", x[0], y[0]);
+	
+	int i, j;
+	int t;
+	for(i = 0; i < n - 1; i++ ){
+		for(j = 0; j <( n - 1 - i); j++){
+			if(y[j] > y[j + 1]){
+			t = x[j];
+			x[j] = x[j + 1];
+			x[j + 1] = t; 
+			t = y[j];
+			y[j] = y[j + 1];
+			y[j + 1] = t;
+			}
+		}
+	}
+	
+	int ans = 0;
+	for(i = 0;i < n&&s >= 0; i++){
+		//printf("x[i]=%d ",x[i]);
+		//printf("y[i]=%d\n",y[i]);
+		if(x[i] > a + b){
+			continue;
+		}
+		else{
+			if(s - y[i] >= 0){
+				s = s - y[i];
+				//printf("s=%d\n",s);
+				ans++;
+			}
+			else if(s == 0){
 				break;
 			}
-			a[i] = N % 10;
-			//printf("a[i]=%d\n", a[i]);
-			N = N / 10;
-			ans++;
-		}			
-		for(int i = 0; ; ans--, i++){
-			if(ans == 0){
-					//printf("%d", anwser);
-					break;
-			}
-				//printf("a[i]=%d\n",a[i]);
-				anwser = anwser * 10 + a[i];		
-				}
-				
-		printf("%d\n", anwser);
-	return 0;   
+		}
+	}
+	printf("%d",ans);
+	return 0;
 }
+
 
 
 
--- recolo111/未命名1.cpp
+++ recolo111/未命名1.cpp
@@ -1,33 +1,58 @@
 #include<stdio.h>
-int main()
-{
-	int  bu = 0;
-	double you = 0, s;
-	scanf("%lf", &s);
-	double a[1000];
-	a[0] = 2;
-	//printf("a[0]=%lf\n", a[0]);
-			
-	if(s <= 2){
-		printf("1");
+int a[10];
+int sum(int i,int a[])
+	{
+		for(int b = 0; i > 0; ){
+               b = i % 10;
+               //printf("b=%d ", b);
+			   a[10]=1;
+			   if(b == 0)
+               a[10] = 0;
+			   i = i / 10;
+			   //printf("i=%d ",i);
+	              for(int c = 1; c <= 9; c++){
+                     if(a[c] == b){
+                        a[c] = 0;
+                        //printf("Ñ­»·Àïa[c]=%d ", a[c]);
+                        break;
+					  }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+	                }
+	   }
+	   return 0;
 	}
 	
-	else{
-		for(int i = 0;;){
-			you = you + a[i];
-			//printf("you = %lf\n",you);
-			
-			bu++;
-			if(you > s){
-			printf("%d\n",bu);
-			break;
-			}
-			i++;	
-			//printf("i = %d\n", i);	
-			a[i] = a[i-1] * 0.98;
-			//printf("a[i] = %lf\n", a[i]);
-		}
- 	return 0;   
+int main()
+{   int  i, ans = 0;
+    int x, y, z, b;
+    int A, B, C;
+	scanf("%d %d %d",&A, &B, &C);
+    
+    for(int i = 0; (C * i) <= 999; i++){
+	 a[1] = 1, a[2] = 2, a[3] = 3;
+	 a[4] = 4, a[5] = 5, a[6] = 6;
+	 a[7] = 7, a[8] = 8, a[9] = 9;
+       x = A * i;
+	   y = B * i;
+	   z = C * i;
+	   sum(x,a);
+	   sum(y,a);
+	   sum(z,a);
+	   if(a[10]==0)
+	   continue;
+	      //printf("a[1]=%d ",a[1]);
+	         if(a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]==0){
+	            printf("%d %d %d\n", x, y, z);
+	            ans++;
+	        }    
 	}
+    if(ans == 0){
+    	printf("No!!!");
+	}
+	return 0 ;
 }
+
+
+
+
+
 
