8eb3d29169dbfa83b136226eaee36306d0f34b54
recolo111 <151364173+recolo111@users.noreply.github.com>
2023-11-26 21:13:27 +0800

    recolo111:p1579
    # 哥德巴赫猜想（升级版）
    
    ## 题目背景
    
    1742 年 6 月 7 日，哥德巴赫写信给当时的大数学家欧拉，正式提出了以下的猜想：任何一个大于 $9$ 的奇数都可以表示成 $3$ 个质数之和。质数是指除了 $1$ 和本身之外没有其他约数的数，如 $2$ 和 $11$ 都是质数，而 $6$ 不是质数，因为 $6$ 除了约数 $1$ 和 $6$ 之外还有约数 $2$ 和 $3$。**需要特别说明的是 $1$ 不是质数。**
    
    这就是哥德巴赫猜想。欧拉在回信中说，他相信这个猜想是正确的，但他不能证明。
    
    从此，这道数学难题引起了几乎所有数学家的注意。哥德巴赫猜想由此成为数学皇冠上一颗可望不可及的“明珠”。
    
    ## 题目描述
    
    现在请你编一个程序验证哥德巴赫猜想。
    
    先给出一个奇数 $n$，要求输出 $3$ 个质数，这 $3$ 个质数之和等于输入的奇数。
    
    ## 输入格式
    
    仅有一行，包含一个正奇数 $n$，其中 $9 < n < 20000$。
    
    ## 输出格式
    
    仅有一行，输出 $3$ 个质数，这 $3$ 个质数之和等于输入的奇数。相邻两个质数之间用一个空格隔开，最后一个质数后面没有空格。如果表示方法不唯一，请输出第一个质数最小的方案，如果第一个质数最小的方案不唯一，请输出第一个质数最小的同时，第二个质数最小的方案。
    
    ## 样例 #1
    
    ### 样例输入 #1
    
    ```
    2009
    ```
    
    ### 样例输出 #1
    
    ```
    3 3 2003
    ```

--- recolo111/test.cpp
+++ recolo111/test.cpp
@@ -1,56 +1,73 @@
 #include<stdio.h>
 int main()
 { 
-	int n, s;
-	scanf("%d %d", &n, &s);//苹果数 n，力气 s。
-	int a, b;
-	//int c;
-	scanf("%d %d", &a, &b);//椅子的高度 a，陶陶手伸直的最大长度 b。
-	//c = a + b;//c是陶陶能碰到最大高度
-	int x[5010], y[5010];
-
-		for(int i = 0; i < n; i++){
-			scanf("%d %d",&x[i], &y[i]);
-			//printf("x[i]=%d y[i]=%d\n", x[i], y[i]);
-		}
-	//printf("x[0]=%d y[0]=%d\n", x[0], y[0]);
-	
-	int i, j;
-	int t;
-	for(i = 0; i < n - 1; i++ ){
-		for(j = 0; j <( n - 1 - i); j++){
-			if(y[j] > y[j + 1]){
-			t = x[j];
-			x[j] = x[j + 1];
-			x[j + 1] = t; 
-			t = y[j];
-			y[j] = y[j + 1];
-			y[j + 1] = t;
-			}
-		}
+	int n;
+	scanf("%d", &n);
+	int a, b, c = 0, t = 0;//a是测试数，t是判断数，c计数；
+	int x[3000];
+	for(int i = 0;i < 3000; i++){
+		x[i] = 0;
 	}
-	
-	int ans = 0;
-	for(i = 0;i < n&&s >= 0; i++){
-		//printf("x[i]=%d ",x[i]);
-		//printf("y[i]=%d\n",y[i]);
-		if(x[i] > a + b){
-			continue;
-		}
-		else{
-			if(s - y[i] >= 0){
-				s = s - y[i];
-				//printf("s=%d\n",s);
-				ans++;
-			}
-			else if(s == 0){
+	for(a = 2; a < n; a++){
+//		printf("a = %d ",a);
+		b = a - 1;
+		t = 0;
+//		printf("b = %d\n",b);
+		
+		for(; b > 1; b--){
+			if((a % b) == 0){  //判断不是素数；                                                                                                                      
+				t = 1;
 				break;
 			}
+//			printf("b = %d\n",b);
 		}
+		
+		if(t == 1){
+			continue;
+		}	
+		if(a == 2 || t == 0){
+//			printf("a = %d ",a);
+			x[c] = a;
+//			printf("c = %d ", c);
+//			printf("x[c] = %d \n",x[c]);
+			c++;
+		}
+		
+	}	//素数排列结束。
+	
+	int i, j, k;
+	int ans = 0;
+	for( i = 0; i < c; i++){
+		for( j = 0; j < c; j++){
+			for( k = 0; k < c; k++){
+				if(x[i] + x[j] + x[k] == n&&x[i] > 0&&x[j] > 0&&x[k] > 0){
+//					printf("i = %d j = %d k = %d\n", i, j, k);
+//					printf("x[i] = %d x[j] = %d x[k] = %d\n", x[i], x[j], x[k]);
+					printf("%d %d %d", x[i], x[j], x[k]);
+					ans = 1;
+					break;	
+				}
+			}
+		if(ans == 1){
+			break;
+		}
+		}
+	if(ans == 1){
+		break;
 	}
-	printf("%d",ans);
+	}
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
