3bbfac36c9ae4ebf0a9a8d6d833b0e41d846f46b
recolo111 <151364173+recolo111@users.noreply.github.com>
2023-12-01 19:30:25 +0800

    recolo111:p1152
    # 欢乐的跳
    
    ## 题目描述
    
    一个 $n$ 个元素的整数数组，如果数组两个连续元素之间差的绝对值包括了 $[1,n-1]$ 之间的所有整数，则称之符合“欢乐的跳”，如数组 $\{1,4,2,3\}$ 符合“欢乐的跳”，因为差的绝对值分别为：$3,2,1$。
    
    给定一个数组，你的任务是判断该数组是否符合“欢乐的跳”。
    
    ## 输入格式
    
    每组测试数据第一行以一个整数 $n(1 \le n \le 1000)$ 开始，接下来 $n$ 个空格隔开的在 $[-10^8,10^8]$ 之间的整数。
    
    ## 输出格式
    
    对于每组测试数据，输出一行若该数组符合“欢乐的跳”则输出 `Jolly`，否则输出 `Not jolly`。
    
    ## 样例 #1
    
    ### 样例输入 #1
    
    ```
    4 1 4 2 3
    ```
    
    ### 样例输出 #1
    
    ```
    Jolly
    ```
    
    ## 样例 #2
    
    ### 样例输入 #2
    
    ```
    5 1 4 2 -1 6
    ```
    
    ### 样例输出 #2
    
    ```
    Not jolly
    ```
    
    ## 提示
    
    $1 \le n \le 1000$

--- recolo111/test.cpp
+++ recolo111/test.cpp
@@ -1,44 +1,47 @@
+#include<iostream>
 #include<stdio.h>
-int main()
-{   
-	int n = 0, ant=0;
-	scanf("%d", &n);
-	int a[10000];
-	for(int i = 0; i < n; i++){
-		if((i+1) == n){
-			scanf("%d",&a[i]);
-		}
-		else{
-			scanf("%d ", &a[i]);
-		}
-		
-//		printf("i=%d ", i);
-//		printf("a[i]=%d\n", a[i]);
-	}
-	int v[10000], k;
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			for( k = 0; k < n; k++){
-//				printf("a[i]=%d a[j]=%d a[k]=%d\n",a[i],a[j],a[k]);
-				if(a[i] == (a[j] + a[k])&&j != k&&j != i&&k != i){
-					ant++;
-					v[k] = 1;
-					break;
-					printf("a[i]=%d a[j]=%d a[k]=%d\n",a[i],a[j],a[k]);
-					printf("ant=%d\n",ant);
-				}
-			}
-			if(v[k] == 1){
-				break;
-			}
-		}
-		if(v[k] == 1){
-			v[k] = 0;
-			continue;
-		}
-		
+#include<algorithm>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+//	cout << n << endl;
+	int i, a[1000];
+	for(i = 0; i < n; i++){
+		cin >> a[i];
+//		cout << a[i] <<endl;
 	}
 	
-	printf("%d", ant);
+	
+	int b[1000];
+	for(i = 1; i < n; i++){
+		if(a[i] - a[i - 1] < 0){
+			b[i] = - (a[i] - a[i - 1]);
+		}
+		else{
+			b[i] = a[i] - a[i - 1];
+		}
+//		cout << b[i] <<endl;
+	}
+	
+	sort(b + 1, b + n);
+	for(i = 1; i < n; i++){
+		//cout << b[i] <<endl;
+	}
+	
+	int flag;
+	for(flag = 1, i = 1; i < n; i++){
+		if(b[i] != i){
+			flag = 0;
+		}
+	}
+	if(flag == 1){
+		cout << "Jolly";
+	}
+	else{
+		cout << "Not jolly";
+	}
+	
 	return 0;
 }
