9e8035ae9e8155dc2897946210131d02615f2ddb
recolo111 <151364173+recolo111@users.noreply.github.com>
2023-12-03 17:53:43 +0800

    recolo111:p1051
    # [NOIP2005 提高组] 谁拿了最多奖学金
    
    ## 题目描述
    
    某校的惯例是在每学期的期末考试之后发放奖学金。发放的奖学金共有五种，获取的条件各自不同：
    
    1. 院士奖学金，每人 $8000$ 元，期末平均成绩高于 $80$ 分（$>80$），并且在本学期内发表$1$篇或$1$篇以上论文的学生均可获得；
    2. 五四奖学金，每人 $4000$ 元，期末平均成绩高于 $85$ 分（$>85$），并且班级评议成绩高于 $80$ 分（$>80$）的学生均可获得；
    3. 成绩优秀奖，每人 $2000$ 元，期末平均成绩高于 $90$ 分（$>90$）的学生均可获得；
    4. 西部奖学金，每人 $1000$ 元，期末平均成绩高于 $85$ 分（$>85$）的西部省份学生均可获得；
    5. 班级贡献奖，每人 $850$ 元，班级评议成绩高于 $80$ 分（$>80$）的学生干部均可获得；
    
    只要符合条件就可以得奖，每项奖学金的获奖人数没有限制，每名学生也可以同时获得多项奖学金。例如姚林的期末平均成绩是 $87$ 分，班级评议成绩 $82$ 分，同时他还是一位学生干部，那么他可以同时获得五四奖学金和班级贡献奖，奖金总数是 $4850$ 元。
    
    现在给出若干学生的相关数据，请计算哪些同学获得的奖金总数最高（假设总有同学能满足获得奖学金的条件）。
    
    ## 输入格式
    
    第一行是$1$个整数 $N$，表示学生的总数。
    
    接下来的 $N$ 行每行是一位学生的数据，从左向右依次是姓名，期末平均成绩，班级评议成绩，是否是学生干部，是否是西部省份学生，以及发表的论文数。姓名是由大小写英文字母组成的长度不超过 $20$ 的字符串（不含空格）；期末平均成绩和班级评议成绩都是 $0$ 到 $100$ 之间的整数（包括 $0$ 和 $100$）；是否是学生干部和是否是西部省份学生分别用 $1$ 个字符表示，$\tt Y$ 表示是，$\tt N$ 表示不是；发表的论文数是 $0$ 到 $10$ 的整数（包括 $0$ 和 $10$）。每两个相邻数据项之间用一个空格分隔。
    
    ## 输出格式
    
    共 $3$ 行。
    
    - 第 $1$ 行是获得最多奖金的学生的姓名。如果有两位或两位以上的学生获得的奖金最多，输出他们之中在输入文件中出现最早的学生的姓名。
    - 第 $2$ 行是这名学生获得的奖金总数。
    - 第 $3$ 行是这 $N$ 个学生获得的奖学金的总数。
    
    ## 样例 #1
    
    ### 样例输入 #1
    
    ```
    4
    YaoLin 87 82 Y N 0
    ChenRuiyi 88 78 N Y 1
    LiXin 92 88 N N 0
    ZhangQin 83 87 Y N 1
    ```
    
    ### 样例输出 #1
    
    ```
    ChenRuiyi
    9000
    28700
    ```
    
    ## 提示
    
    **【数据范围】**
    
    对于 $100\%$ 的数据，满足 $1 \le  N \le  100$。
    
    **【题目来源】**
    
    NOIP 2005 提高组第一题

--- recolo111/test.cpp
+++ recolo111/test.cpp
@@ -1,26 +1,65 @@
 #include<iostream>
 #include<stdio.h>
 #include<algorithm>
+#include<string>
 using namespace std;
 
-int main(){
-	int a[4];
-	for(int i = 0; i < 3; i++){
-		cin >> a[i];
-//		cout << a[i] <<endl;
+struct student{
+	string name;
+	int gardeping;
+	int gardeban;
+	char ganbu;
+	char xibu;
+	int lunwen;
+	int ans;
+	int sum;
+}stu[101];
+
+bool map(student a, student b){
+	if(a.ans == b.ans){
+		return a.sum < b.sum;
 	}
-	sort(a, a+3);
-	for(int i = 0; i < 3; i++){
-//		cout << a[i] <<endl;
+	else{
+		return a.ans > b.ans;
+	}
+};
+
+int main(){
+	int N;
+	cin >> N;
+	
+	for(int i = 0; i < N; i++){
+		cin >> stu[i].name >> stu[i].gardeping >> stu[i].gardeban >> stu[i].ganbu >> stu[i].xibu >> stu[i].lunwen;
+		stu[i].sum = i;
 	}
 	
-	int max;
-	for(max = a[0]; max > 0; max--){
-		if(a[2] % max ==0&&a[0] % max == 0){
-			break;
+	int ans, answer = 0;
+	for(int i = 0; i < N; i++){
+		ans = 0;
+		if(stu[i].gardeping > 80 && stu[i].lunwen >= 1){
+			ans = ans + 8000;
 		}
+		if(stu[i].gardeping > 85 && stu[i].gardeban > 80){
+			ans = ans + 4000;
+		}
+		if(stu[i].gardeping > 90){
+			ans = ans + 2000;
+		}
+		if(stu[i].gardeping > 85 && stu[i].xibu == 'Y'){
+			ans = ans + 1000;
+		}
+		if(stu[i].gardeban > 80 && stu[i].ganbu == 'Y'){
+			ans = ans + 850;
+		}
+		stu[i].ans = ans;
+		answer = answer + ans;
 	}
-//	cout << 'max=' << max << endl;
-	cout << a[0]/max << '/' << a[2]/max <<endl;
+	
+	sort(stu, stu + N, map);
+	cout << stu[0].name << endl << stu[0].ans << endl << answer;
 	return 0;
 }
+
+
+
+
