0b8bb60ab4907a5bdb8717318f6fcc85119e00ed
recolo111 <151364173+recolo111@users.noreply.github.com>
2023-12-14 20:06:19 +0800

    recolo111:p1163
    # 银行贷款
    
    ## 题目描述
    
    当一个人从银行贷款后，在一段时间内他（她）将不得不每月偿还固定的分期付款。这个问题要求计算出贷款者向银行支付的利率。假设利率按月累计。
    
    ## 输入格式
    
    三个用空格隔开的正整数。
    
    第一个整数表示贷款的原值 $w_0$，第二个整数表示每月支付的分期付款金额 $w$，第三个整数表示分期付款还清贷款所需的总月数 $m$。
    
    ## 输出格式
    
    一个实数，表示该贷款的月利率（用百分数表示），四舍五入精确到 $0.1\%$。
    
    数据保证答案不超过 $300.0\%$。
    
    ## 样例 #1
    
    ### 样例输入 #1
    
    ```
    1000 100 12
    ```
    
    ### 样例输出 #1
    
    ```
    2.9
    ```
    
    ## 提示
    
    数据保证，$1 \leq w_0, w\leq 2^{31}-1$，$1 \leq m\leq 3000$。

--- recolo111/test.cpp
+++ recolo111/test.cpp
@@ -1,42 +1,28 @@
-#include<iostream>
 #include<algorithm>
+#include<iostream>
 #include<stdio.h>
 using namespace std;
 
-struct studet{
-	int xuehao;
-	int chinese;
-	int math;
-	int english;
-	int endgarde;
-}stu[301];
-
-bool map(studet a , studet b){
-	if(a.endgarde == b.endgarde){
-		if(a.chinese == b.chinese){
-			return a.xuehao  < b.xuehao;
+int main(){
+	//第一个整数表示贷款的原值 ，第二个整数表示每月支付的分期付款金额 ，第三个整数表示分期付款还清贷款所需的总月数 
+	double w, w0, www;
+	int m;	
+	cin >> w >> w0 >> m;
+	double l = 0, r = 500, middle;
+	while(r - l > 0.0001){
+		middle = (r + l) / 2;
+//		printf("middle = %lf\n", middle);
+		www = w;
+		for(int i = 0; i < m; i++){
+			www = www + www * (middle / 100) - w0; 
+		}
+		
+		if(www > 0.0001){
+			r = middle;
 		}
 		else{
-			return a.chinese > b.chinese;
+			l = middle;
 		}
 	}
-	else{
-		return a.endgarde > b.endgarde;
-	}
-}
-
-int main(){
-	int n;
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		stu[i].xuehao = i + 1;
-		cin >> stu[i].chinese >> stu[i].math >> stu[i].english;
-		stu[i].endgarde = stu[i].chinese + stu[i].math + stu[i].english;
-	}
-	
-	sort(stu, stu + n, map);
-	
-	for(int i = 0; i < 5; i++){
-		cout << stu[i].xuehao <<' '<< stu[i].endgarde << endl;
-	}
+	printf("%.1lf", l);
 }
